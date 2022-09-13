a667bede1fc36b1785c6b9e9df251615ff51fa81
Hassan Javed <hassanjaved917127@gmail.com>
2022-09-14 02:27:15 +0500

    some logic added v-8

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,16 +11,33 @@ const TextForm = (props) => {
         setText(event.target.value);
 
     }
+    const handleDelClick = () =>{
+        setText("");
+    }
+    const handleLowClick = ()=>{
+        setText(text.toLowerCase());
+    }
     const [text, setText] = useState("Enter Text here");
     return (
-
-        <div>
+        <>
+        <div class = "container">
             <h1>{props.heading}</h1>
             <div className="form-group">
                 <textarea className="form-control" value = {text} onChange = {handleOnChange} id="mybox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-secondary mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
+            <button className="btn btn-danger " onClick={handleDelClick}>Remove</button>
         </div>
+        <div className="container my-3">
+            <h2>Your Text Summary</h2>
+            <p>{text.split(" ")[text.split(" ").length - 1] === "" ? text.split(" ").length - 1 : text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+
+        </div>
+        </>
     );
 }
 
