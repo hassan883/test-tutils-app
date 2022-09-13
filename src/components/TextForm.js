ef49a637f74e4cf9c64fd6b0be4b3138d5ba02df
Hassan Javed <hassanjaved917127@gmail.com>
2022-09-13 21:53:23 +0500

    state v-7 completed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,19 +4,23 @@ const TextForm = (props) => {
 
     const handleUpClick = ()=>{
         console.log("Handle on click is Clicked");
+        setText(text.toUpperCase());
     }
-    const handleOnChange = ()=>{
+    const handleOnChange = (event)=>{
         console.log("Handle on Change");
+        setText(event.target.value);
+
     }
     const [text, setText] = useState("Enter Text here");
     return (
-        <form>
+
+        <div>
             <h1>{props.heading}</h1>
             <div className="form-group">
-                <textarea className="form-control" value = {text} onChange = {handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
+                <textarea className="form-control" value = {text} onChange = {handleOnChange} id="mybox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
-        </form>
+        </div>
     );
 }
 
