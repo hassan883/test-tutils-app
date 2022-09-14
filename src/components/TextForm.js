27f75e60086b878c687363c31d284da4b2f1f564
Hassan Javed <hassanjaved917127@gmail.com>
2022-09-14 10:05:17 +0500

    adding some functionalities extra button v-11

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -17,6 +17,15 @@ const TextForm = (props) => {
     const handleLowClick = ()=>{
         setText(text.toLowerCase());
     }
+    const handleCopy = ()=>{
+        let text = document.getElementById('mybox');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    const handleExtraSpaces = ()=>{
+        const newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
     const [text, setText] = useState("Enter Text here");
     return (
         <>
@@ -25,9 +34,13 @@ const TextForm = (props) => {
             <div className="form-group">
                 <textarea className="form-control" value = {text} onChange = {handleOnChange} id="mybox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
-            <button className="btn btn-secondary mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
-            <button className="btn btn-danger " onClick={handleDelClick}>Remove</button>
+            <button className="btn btn-primary my-3" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-secondary mx-2 my-3" onClick={handleLowClick}>Convert to LowerCase</button>
+            <button className="btn btn-danger my-3" onClick={handleDelClick}>Remove</button>
+            <button className="btn btn-success mx-2 my-3" onClick={handleCopy}>Copy Text</button>
+            <button className="btn btn-success  my-3" onClick={handleExtraSpaces}>Remove Spaces</button>
+
+
         </div>
         <div className="container my-3">
             <h2>Your Text Summary</h2>
